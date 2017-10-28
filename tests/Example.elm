b2419c11d1f00be489e6a145ee64e5fda84143f3
Franco Ponticelli <franco.ponticelli@gmail.com>
2017-10-28 15:24:41 -0600

    add UDate functions

--- tests/Example.elm
+++ tests/Example.elm
@@ -31,16 +31,60 @@ suite =
       , test "Twitter Default" <|
         testDecode """{ "type" : "twitter", "value" : "t" }""" socialMediaWithPriorityDecoder ({ handle = Twitter "t", priority = Primary })
       ]
+    , describe "Test UDate building"
+      [ test "Year" <|
+        \() -> Expect.equal (uDateToString (year 2017)) "2017"
+      , test "Month" <|
+        \() -> Expect.equal (uDateToString (month 2017 10)) "2017-10"
+      , test "Month out of range (positive)" <|
+        \() -> Expect.equal (uDateToString (month 2015 25)) "2017-01"
+      , test "Month out of range (zero)" <|
+        \() -> Expect.equal (uDateToString (month 2018 0)) "2017-12"
+      , test "Month out of range (negative)" <|
+        \() -> Expect.equal (uDateToString (month 2018 -2)) "2017-10"
+      , test "Day" <|
+        \() -> Expect.equal (uDateToString (day 2017 10 28)) "2017-10-28"
+      , test "Day out of range (positive)" <|
+        \() -> Expect.equal (uDateToString (day 2017 2 29)) "2017-03-01"
+      , test "Day out of range (zero)" <|
+        \() -> Expect.equal (uDateToString (day 2018 0 0)) "2017-11-30"
+      , test "Day out of range (negative)" <|
+        \() -> Expect.equal (uDateToString (day 2018 1 -2)) "2017-12-29"
+      ]
+    , describe "Test UDate parsing"
+      [ test "Year" <|
+        testResult (uDateParse "2017") (year 2017)
+      , test "Month" <|
+        testResult (uDateParse "2017-10") (month 2017 10)
+      , test "Day" <|
+        testResult (uDateParse "2017-10-28") (day 2017 10 28)
+      ]
     ]
+
+assertResult : Result String a -> a -> Expectation
+assertResult r a =
+  case r of
+      Ok value -> Expect.equal a value
+      Err f -> Expect.fail f
+
+expectErrResult : Result String a -> Expectation
+expectErrResult r =
+  case r of
+      Ok value -> Expect.fail <| "result was expected to be error but it is Ok with: " ++ (toString value)
+      Err f -> Expect.pass
 
 assertDecode : String -> Decoder a -> a -> Expectation
 assertDecode s d a =
   let
     decoded = decodeString d s
   in
-    case decoded of
-      Ok value -> Expect.equal a value
-      Err f -> Expect.fail f
+    assertResult decoded a
 
 testDecode : String -> Decoder a -> a -> (() -> Expectation)
 testDecode s d a = \() -> assertDecode s d a
+
+testResult : Result String a -> a -> (() -> Expectation)
+testResult r a = \() -> assertResult r a
+
+testErrResult : Result String a -> (() -> Expectation)
+testErrResult r = \() -> expectErrResult r
